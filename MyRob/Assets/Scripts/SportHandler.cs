e50eabab991ceb076d903d12514c347da40841c8
jesseleppens <jesseleppens@hotmail.com>
2021-11-10 13:39:47 +0100

    SPORT added functionality

--- MyRob/Assets/Scripts/SportHandler.cs
+++ MyRob/Assets/Scripts/SportHandler.cs
@@ -8,7 +8,10 @@ public class SportHandler : MonoBehaviour
     bool isActive = false;
     public Animator animator;
     public GameObject panel;
-
+    public InputField[] minutesArray;
+    public Text totalCaloriesText;
+    float[] caloriesArray = {8.1F, 14.9F, 7.9F, 6.5F, 9.4F, 1.6F, 2.6F, 2.2F, 2.0F};
+    int totalCalorie;
 
     public void OnButtonClick()
     {
@@ -24,9 +27,20 @@ public class SportHandler : MonoBehaviour
             animator.Play("SportPopup_End");
             isActive = false;
         }
-
     }
 
-
-
+    public void CalorieCalculate()
+    {
+        for (int i = 0; i < minutesArray.Length; i++)
+        {
+            if (minutesArray[i].text == "" )
+            {
+                continue;
+            }
+            totalCalorie += Mathf.RoundToInt(caloriesArray[i] * System.Convert.ToInt32(minutesArray[i].text));
+        }
+        StatsTracker.sport = totalCalorie;
+        StatsTracker.StatsBubbleUpdate();
+        Debug.Log(totalCalorie);
+    }
 }
